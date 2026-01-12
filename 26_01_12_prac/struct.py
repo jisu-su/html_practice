a9936872dbf4758105759912af02d57c1e21cc4b
jisu-su <trleresoo@gmail.com>
2026-01-12 12:06:35 +0900

    Delete 26_01_12_prac/struct.py

--- 26_01_12_prac/struct.py
+++
@@ -1,9 +0,0 @@
-practics = {
-    "left_angle_bracket": "<",
-    "right_angle_bracket": ">",
-    "tag_name": "",
-    "close_tag_slash": "/",
-    "children_elements": []
-}
-
-print(f"{practics['left_angle_bracket']}공욱재{practics['right_angle_bracket']}나야{practics['left_angle_bracket']}{practics['close_tag_slash']}공욱재{practics['right_angle_bracket']}")
