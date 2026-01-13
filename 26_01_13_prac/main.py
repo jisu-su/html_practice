ce9c3b39a5dd5f5e4c1e3894bd760bb42d076421
박지수 <trleresoo@gmail.com>
2026-01-13 11:32:48 +0900

    최대한 노란색 없게 만들기

--- 26_01_13_prac/main.py
+++ 26_01_13_prac/main.py
@@ -1,15 +1,18 @@
 
 tag_name = "h1"
-angle_bracket = ["<", ">"]
 content = "고양이"
 
 
+
 def make_tag(isOpen):
+    angle_bracket = ["<", ">"]
+    slash = "/"
+
     if isOpen == True:
         open = angle_bracket[0] + tag_name + angle_bracket[1]
         return open
     else:
-        close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
+        close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
         return close
 
 
