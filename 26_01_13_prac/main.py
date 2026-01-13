2d61b3619815a8596dca8f5df50b13b145ccca01
박지수 <trleresoo@gmail.com>
2026-01-13 11:23:02 +0900

    조립해보기

--- 26_01_13_prac/main.py
+++ 26_01_13_prac/main.py
@@ -3,7 +3,8 @@ tag_name = "h1"
 angle_bracket = ["<", ">"]
 content = "고양이"
 
-tag_assemble = angle_bracket[0] + tag_name + angle_bracket[1]
+tag_assemble_open = angle_bracket[0] + tag_name + angle_bracket[1]
+tag_assemble_close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
 
-result = tag_assemble + "고양이</h1>"
+result = tag_assemble_open + content + tag_assemble_close
 
