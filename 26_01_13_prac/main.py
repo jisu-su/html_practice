b9ab213678098c485e0f65b456351018a3986ed4
박지수 <trleresoo@gmail.com>
2026-01-13 11:26:51 +0900

    함수 만들어보기 - 매개변수 설정이 어렵다

--- 26_01_13_prac/main.py
+++ 26_01_13_prac/main.py
@@ -6,5 +6,14 @@ content = "고양이"
 tag_assemble_open = angle_bracket[0] + tag_name + angle_bracket[1]
 tag_assemble_close = angle_bracket[0] + "/" + tag_name + angle_bracket[1]
 
+def make_tag(isOpen):
+    if isOpen == True:
+        open = tag_assemble_open
+        return open + content + tag_assemble_close
+    else:
+        close = tag_assemble_close
+        return tag_assemble_open + content + close
+
+
 result = tag_assemble_open + content + tag_assemble_close
 
