5a61468a6b09e5a11b5be9f16b7355d51b3017d0
박지수 <trleresoo@gmail.com>
2026-01-13 11:43:09 +0900

    최종 버전

--- 26_01_13_prac/main.py
+++ 26_01_13_prac/main.py
@@ -1,19 +1,21 @@
 
 content = "고양이"
 
+def make_element(isOpen, tag_name, content):
+    def make_tag(isOpen, tag_name):
+        angle_bracket = ["<", ">"]
+        slash = "/"
 
-def make_tag(isOpen, tag_name):
-    angle_bracket = ["<", ">"]
-    slash = "/"
+        if isOpen == True:
+            open = angle_bracket[0] + tag_name + angle_bracket[1]
+            return open
+        else:
+            close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
+            return close
+    result = make_tag(isOpen, tag_name) + content + make_tag(not isOpen, tag_name)
+    return result 
 
-    if isOpen == True:
-        open = angle_bracket[0] + tag_name + angle_bracket[1]
-        return open
-    else:
-        close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
-        return close
 
-for _ in range(100):
-    result = make_tag(True, "h1:") + content + make_tag(False, "h1")
-    print(result)
+result_element = make_element(True, "h1", content)
+print(result_element)
 
