dffd66d0da0713af316539cbd2924e076e5a7771
박지수 <trleresoo@gmail.com>
2026-01-13 11:37:05 +0900

    반복해서 100번만 만들어보기

--- 26_01_13_prac/main.py
+++ 26_01_13_prac/main.py
@@ -13,7 +13,7 @@ def make_tag(isOpen, tag_name):
         close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
         return close
 
-
-result = make_tag(True, "h1:") + content + make_tag(False, "h1")
-print(result)
+for _ in range(100):
+    result = make_tag(True, "h1:") + content + make_tag(False, "h1")
+    print(result)
 
