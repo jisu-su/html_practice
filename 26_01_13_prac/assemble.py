f7532785963a9025bf2b63049e95d6a3a5bf5b6b
박지수 <trleresoo@gmail.com>
2026-01-15 17:52:02 +0900

    stage 2: 패턴 발견 완료

--- 26_01_13_prac/assemble.py
+++ 26_01_13_prac/assemble.py
@@ -10,6 +10,14 @@ slash = "/"
 result = "<" + tag_name + ">" + content + "<" + slash + tag_name + ">"
 
 # Stage 2: 패턴 발견
+tag_name = "p"
+content = "Python is fun!"
+angle_bracket = ["<", ">"]
+slash = "/"
 
+open = angle_bracket[0] + tag_name + angle_bracket[1]
+close = angle_bracket[0] + slash + tag_name + angle_bracket[1]
+
+result = open + content + close
 
 # Stage 7까지 진화시켜보세요!
